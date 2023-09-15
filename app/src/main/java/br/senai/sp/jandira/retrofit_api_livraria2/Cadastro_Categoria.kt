1e9c587f8e281fdd5e3f2ea9dbcada3edab96d88
MÔNICA RIBEIRO DA SILVA <22282167@dsn127m1241955.cfp127aluno.local>
2023-09-15 07:41:18 -0300

    CRUD com a api livraria

---
+++ app/src/main/java/br/senai/sp/jandira/retrofit_api_livraria2/Cadastro_Categoria.kt
@@ -0,0 +1,11 @@
+package br.senai.sp.jandira.retrofit_api_livraria2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Cadastro_Categoria : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cadastro_categoria)
+    }
+}
