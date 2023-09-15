64bd0c267654cb47635fa680dc16539c4ead4bf8
MÔNICA RIBEIRO DA SILVA <22282167@dsn127m1241955.cfp127aluno.local>
2023-09-15 09:08:45 -0300

    atualizado

--- app/src/main/java/br/senai/sp/jandira/retrofit_api_livraria2/Cadastro_Categoria.kt
+++ app/src/main/java/br/senai/sp/jandira/retrofit_api_livraria2/Cadastro_Categoria.kt
@@ -2,10 +2,30 @@ package br.senai.sp.jandira.retrofit_api_livraria2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class Cadastro_Categoria : AppCompatActivity() {
+
+    private lateinit var apiService: ApiService
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cadastro_categoria)
+
+
+//        Conecta apiservice Com a API rest através da classe RetrofitHelper
+//        e seu método getInstance
+        apiService = RetrofitHelper.getInstance().create(ApiService::class.java)
+
+//        Recupera o componente gráfico de edittext
+        val txtCategoria = findViewById<EditText>(R.id.txtCategoria)
+
+//        trata a ação de clique ou toque no botao cadastrar
+        findViewById<Button>(R.id.btnCadastrarCategoria).setOnClickListener {
+
+//            Recuperar o dado digitado pelo usuario
+              val nomeCategoria = txtCategoria.text
+//            Enviar a requisição de cadastro para a api
+        }
     }
 }
---
+++ app/src/main/java/br/senai/sp/jandira/retrofit_api_livraria2/RetrofitHelper.kt
@@ -0,0 +1,17 @@
+package br.senai.sp.jandira.retrofit_api_livraria2
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitHelper {
+
+    private const val baseurl = "https://localhost:3000"
+
+    fun getInstance(): Retrofit {
+        return Retrofit.Builder ()
+            .baseUrl(baseurl)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+}
